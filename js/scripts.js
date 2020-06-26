718a5155f8f954eedfdab98c7d2cada1ca008999
Victoria Marques <vicck.rj@gmail.com>
2020-06-26 12:40:49 -0300

    codificando...

--- js/scripts.js
+++ js/scripts.js
@@ -1,73 +1,48 @@
-
-//objeto calculadora
 let calculadora = {
-   display: document.querySelector("#display"),
-   botoes: document.querySelectorAll("div > button"),
+    display: document.querySelector("#display"),
+
 };
 
-
-
-let botaoUm = document.querySelector("#numeroUm");
-let botaoSoma = document.querySelector("#soma");
-let botaoIgual = document.querySelector("#igual");
-
-let acumulador = "";
-
-let historico = "";
-
-// botaoUm.onclick = function() {
-//     let display = calculadora.display.innerText += 1;
-// }
-
-
-
-// botaoSoma.onclick = function() {
-//     acumulador += calculadora.display.innerText;
-//     acumulador += " + ";
-//     console.log(acumulador)
-//      calculadora.display.innerText = "";
-// }
-
-// botaoIgual.onclick = function() {
-//     //TODO: checar se existe conteúdo dentro do display
-  
-//     //colocando conteúdo do display no acumulador
-//     acumulador += calculadora.display.innerText;
-//     //resolvendo conta (conteúdo acumulador)
-//     let resultado = eval(acumulador);
-//     //colocando no display o conteúdo do acumulador
-//     calculadora.display.innerText = resultado;
-//     // limpando o acumulador
-//     //acumulador = "";
-
-// }
+let valorBotao; 
 
 let botoes = document.querySelectorAll("div > button");
- 
+
+let botaoZero = document.querySelector("#numeroZero");
+let botaoUm = document.querySelector("#numeroUm");
+let botaoDois = document.querySelector("#numeroDois");
+let botaoTres = document.querySelector("#numeroTres");
+let botaoQuatro = document.querySelector("#numeroQuatro");
+let botaoCinco = document.querySelector("#numeroCinco");
+let botaoSeis = document.querySelector("#numeroSeis");
+let botaoSete = document.querySelector("#numeroSete");
+let botaoOito = document.querySelector("#numeroOito");
+let botaoNove = document.querySelector("#numeroNove");
+
+let botaoSoma = document.querySelector("#soma");
+let botaoSubtracao = document.querySelector("#subtracao");
+let botaoDivisao = document.querySelector("#divisao");
+let botaoMultiplicacao = document.querySelector("#multiplicacao");
+let botaoIgual = document.querySelector("#igual");
+let botaoApagar = document.querySelector("#apagar");
+
+
 botoes.forEach(function(botao){
     botao.addEventListener("click", function() {
-        let btnSoma = document.querySelector("#soma");
-        let btnDivisao = document.querySelector("#divisao");
-        let btnMultiplicao = document.querySelector("#multiplicacao");
-        let btnSubstracao = document.querySelector("#subtracao");
-        let btnIgual = document.querySelector("#igual");
-
-    
-
-        let display = calculadora.display.innerText = this.innerHTML;
-    })
-})
-
-botaoSoma.addEventListener("click", function(){
-    historico += calculadora.display.innerText
-    console.log(historico)
+        atribuirValorBotao(botao)
+    });
 });
 
-// function botaoSoma(operador){
+function atribuirValorBotao(botao){
+    valorBotao = botao.textContent;
+    
+    if(botao === botaoSoma){
+        
+    }
+}
 
-// }
-
-// function guardaDigitoDisplay(digito){
 
 
-// }
+
+
+
+
